205529d7213ba72ed1651ca8af47101aedcbacdb
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2021-03-01 16:00:21 +0000

    adding the rest of the changes

--- App.js
+++ App.js
@@ -1,113 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native';
+import {Header} from 'react-native/Libraries/NewAppScreen';
+import {ApolloProvider, gql, useQuery} from '@apollo/client';
+import client from './client';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      results {
+        id
+        name
+      }
+    }
+  }
+`;
 
-const App: () => React$Node = () => {
+export const App = () => (
+  <ApolloProvider client={client}>
+    <MyRootComponent />
+  </ApolloProvider>
+);
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+export const MyRootComponent = () => {
+  const {data, loading, error} = useQuery(GET_CHARACTERS);
+
+  const DATA = data?.characters?.results;
+
+  const renderItem = ({item}) => {
+    return <Item title={item.name} />;
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.sectionTitle}>Rick and morty MSW demo</Text>
+
+        <Header />
+
+        {loading && <ActivityIndicator testID="loading" />}
+
+        {error && <Text testID="error">{`${error}`}</Text>}
+
+        {!loading && !error && DATA?.length > 0 && (
+          <FlatList
+            testID="list"
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        )}
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: Colors.black,
+    color: 'black',
+    marginBottom: 40,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'white',
   },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  title: {
+    fontSize: 32,
   },
 });
 
--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+if (process.env.NODE_ENV === 'development') {
+  require('react-native-url-polyfill/auto');
+  const {native} = require('./mocks/native');
+  native.listen();
+}
+
 AppRegistry.registerComponent(appName, () => App);
